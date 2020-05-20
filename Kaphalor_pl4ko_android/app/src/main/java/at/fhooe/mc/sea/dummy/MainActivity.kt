2646f5f9047577d51afe976eab7c2536f8fb14b7
pl4ko <s1910237019@students.fh-hagenberg.at>
2020-05-20 15:40:22 +0200

    I pushed a dummy Adndoird project

---
+++ Kaphalor_pl4ko_android/app/src/main/java/at/fhooe/mc/sea/dummy/MainActivity.kt
@@ -0,0 +1,12 @@
+package at.fhooe.mc.sea.dummy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
